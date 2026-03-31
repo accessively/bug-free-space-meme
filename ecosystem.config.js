473df8e6b7c019b68604a4423de01235e3a03649
accessively <accessivelysocials@gmail.com>
2026-03-31 11:44:38 +0000

    Add PM2 ecosystem configuration with SMTP settings

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "bug-free-space-meme",
+      script: "npm",
+      args: "run start",
+      env: {
+        SMTP_HOST: "smtp.gmail.com",
+        SMTP_PORT: "587",
+        SMTP_USER: "websitesaccessively@gmail.com",
+        SMTP_PASS: "neqy xqjp thbo dtib",
+        SMTP_FROM: "websitesaccessively@gmail.com",
+        FORM_RECEIVER_EMAIL: "solutions@accessibelybpo.com"
+      }
+    }
+  ]
+};
